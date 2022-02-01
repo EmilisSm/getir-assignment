ebe30bbf3050838dbd976b6e26612fd6d7c33d2c
Emil <emil.simanski@gmail.com>
2022-02-01 07:01:27 +0100

    Add new fn() for fetching data

--- src/api/itemsService.ts
+++ src/api/itemsService.ts
@@ -1,7 +1,7 @@
 const url = 'https://getir-assignment-server.herokuapp.com/'; // for development set to 'http://localhost:3000/'
 
-const fetchProductItems = async (page: number) => {
-  return fetch(`${url}items?_page=${page}&_limit=16`, {
+const clientGetRequest = (urlRequest: string, page: number) => {
+  return fetch(`${url}${urlRequest}?_page=${page}&_limit=16`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -9,41 +9,26 @@ const fetchProductItems = async (page: number) => {
   }).then((response) => response.json());
 };
 
-const sortProductItemsByPriceAsc = (page: number) => {
-  return fetch(`${url}items?_sort=price&_order=asc&_page=${page}&_limit=16`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then((response) => response.json());
-};
+const fetchProductItems = async (page: number) =>
+  clientGetRequest('items', page);
 
-const sortProductItemsByPriceDesc = (page: number) => {
-  return fetch(`${url}items?_sort=price&_order=desc&_page=${page}&_limit=16`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then((response) => response.json());
-};
+const sortProductItemsByPriceAsc = (page: number) =>
+  clientGetRequest('items?_sort=price&_order=asc&', page);
 
-const sortProductItemsByDateAsc = (page: number) => {
-  return fetch(`${url}items?_sort=added&_order=asc&_page=${page}&_limit=16`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then((response) => response.json());
-};
+const sortProductItemsByPriceDesc = (page: number) =>
+  clientGetRequest('items?_sort=price&_order=desc&', page);
 
-const sortProductItemsByDateDesc = (page: number) => {
-  return fetch(`${url}items?_sort=added&_order=desc&_page=${page}&_limit=16`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then((response) => response.json());
-};
+const sortProductItemsByDateAsc = (page: number) =>
+  clientGetRequest('items?_sort=added&_order=asc&', page);
+
+const sortProductItemsByDateDesc = (page: number) =>
+  clientGetRequest('items?_sort=added&_order=desc&', page);
+
+const filterByProductType = (
+  page: number,
+  productType: string
+  // isSorted: boolean
+) => clientGetRequest(`items?itemType=${productType}&`, page);
 
 const ItemsService = {
   fetchProductItems,
@@ -51,6 +36,7 @@ const ItemsService = {
   sortProductItemsByPriceDesc,
   sortProductItemsByDateAsc,
   sortProductItemsByDateDesc,
+  filterByProductType,
 };
 
 export default ItemsService;
